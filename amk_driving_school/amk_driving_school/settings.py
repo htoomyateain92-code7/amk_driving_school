023e8260ff8bed045764cbd88458bbdbd327d526
htoomyateain92-code7 <htoomyateain92@gmail.com>
2025-11-30 12:52:14 +0630

    Update settings.py

--- amk_driving_school/amk_driving_school/settings.py
+++ amk_driving_school/amk_driving_school/settings.py
@@ -139,6 +139,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
