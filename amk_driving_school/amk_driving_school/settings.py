06e8f530a680a70490515535e271d63e63a33853
htoomyateain92-code7 <htoomyateain92@gmail.com>
2025-11-30 12:34:00 +0630

    Update settings.py

--- amk_driving_school/amk_driving_school/settings.py
+++ amk_driving_school/amk_driving_school/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
