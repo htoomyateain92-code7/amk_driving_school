839c04c6bcc4ccd5d5052fdd5a512abf3373923b
htoomyateain92-code7 <htoomyateain92@gmail.com>
2025-11-30 12:50:41 +0630

    Update settings.py

--- amk_driving_school/amk_driving_school/settings.py
+++ amk_driving_school/amk_driving_school/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'amk_driving_school.urls'
